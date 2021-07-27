391f96327055009dd7d68781c0f8baa8f306e16b
RexKenley <rexkenley@hotmail.com>
2021-07-27 15:33:12 -0400

    Add multi snapshot

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.[tj]sx?$": "babel-jest",
     "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx",
+    "^.+\\.stories\\.[tj]sx?$": "@storybook/addon-storyshots/injectFileName",
     "^.+\\.svg$": "jest-svg-transformer",
     ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
   }
--- src/stories/storyshots.test.ts
+++ src/stories/storyshots.test.ts
@@ -1,2 +1,7 @@
-import initStoryshots from "@storybook/addon-storyshots";
-initStoryshots();
+import initStoryshots, {
+  multiSnapshotWithOptions
+} from "@storybook/addon-storyshots";
+initStoryshots({
+  integrityOptions: { cwd: __dirname },
+  test: multiSnapshotWithOptions()
+});

81d898e8a6503a11d83a91d977a1c90346ecf6ca
Markus Heinrichs <markus@heinrichs.es>
2021-07-30 17:45:09 +0200

    Fix test isssue for Introduction.stories

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "^.+\\.[tj]sx?$": "babel-jest",
     "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx",
     "^.+\\.svg$": "jest-svg-transformer",
-    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "jest-transform-stub"
   }
 };
